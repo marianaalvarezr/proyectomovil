239f79ec3ef433f8fd46d758105e6560d95d769a
Santiago <Santyfomezduque30@gmail.com>
2023-06-21 19:57:46 -0500

    Se utilizo java por primera vez

--- app/src/main/java/com/example/puebliando/MainActivity.java
+++ app/src/main/java/com/example/puebliando/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.puebliando;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class MainActivity extends AppCompatActivity {
+    //Atributos=elementos de la actividad
+    MediaPlayer cancion;
+
+    //Metodos
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cancion=MediaPlayer.create(this,R.raw.baiana);
+        cancion.start();
+        //temporizamos el splash
+        TimerTask inicioAPP=new TimerTask() {
+            @Override
+            public void run() {
+               //activo el home
+                Intent lanzamiento=new Intent(MainActivity.this,Home.class);
+                startActivity(lanzamiento);
+            }
+        };
+        Timer tiempo=new Timer();
+        tiempo.schedule(inicioAPP,10000);
     }
 }
